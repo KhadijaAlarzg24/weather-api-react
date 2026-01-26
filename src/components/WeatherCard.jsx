808332feae88ea35bcbcadf17f81a1e57a581a32
Khadija <khadija.alarzgue@gmail.com>
2026-01-26 21:43:13 +0100

    docs: translate comments to English

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -15,13 +15,13 @@ function WeatherCard({ weather }) {
     >
       <h2 className="text-2xl font-bold text-center">{weather.city}</h2>
 
-      {/* قسم درجة الحرارة - جعلناه ضخماً وعريضاً جداً */}
+      {/* Temperature section  */}
 <div className="flex items-center justify-center gap-3 text-6xl font-black text-gray-900">
   <Thermometer className="w-10 h-10 stroke-[3px]" /> {/* زيادة سُمك حدود الأيقونة */}
   {weather.temp}°C
 </div>
 
-{/* قسم وصف الطقس - جعلناه عريضاً وواضحاً */}
+{/* Weather description section  */}
 <p className="text-center font-bold capitalize flex items-center gap-3 justify-center mt-2 text-gray-700">
   <Cloud className="w-7 h-7 stroke-[2px]" />
   {weather.desc}
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/WeatherApp/",   // هذا مهم جدًا
+  base: "/WeatherApp/",
   plugins: [react(), tailwindcss()],
 })
